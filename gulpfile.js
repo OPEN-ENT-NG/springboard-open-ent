f82b109fa0fcff5c87ccdd0e6162a48575e12a5a
Lauren Ceccotti <lauren.ceccotti@web-education.net>
2015-12-04 17:24:37 +0100

    [evo] tech clean - push custom gulp tasks for local testing on themes

--- gulpfile.js
+++ gulpfile.js
@@ -16,3 +16,26 @@ gulp.task('theme-leo', ['update-libs'], function () {
       .pipe(less())
       .pipe(gulp.dest('./assets/themes/leo/dyslexic'));
 });
+
+//copy themes elements on local to see tests
+gulp.task('copy-theme-leo', function () {
+    return gulp.src(['../theme-leo/**/*'])
+     .pipe(gulp.dest('./assets/themes/leo'));
+});
+
+//copy themes elements on local to see tests
+gulp.task('copy-css-lib', function () {
+   return  gulp.src(['../css-lib/**/*'])
+    .pipe(gulp.dest('./assets/themes/entcore-css-lib'));
+});
+
+// compile local css to test
+gulp.task('compile-leo-css', ['copy-theme-leo', 'copy-css-lib'], function () {
+   gulp.src('./assets/themes/leo/default/*.less')
+     .pipe(less())
+     .pipe(gulp.dest('./assets/themes/leo/default'));
+
+   gulp.src('./assets/themes/leo/dyslexic/*.less')
+     .pipe(less())
+     .pipe(gulp.dest('./assets/themes/leo/dyslexic'));
+});
