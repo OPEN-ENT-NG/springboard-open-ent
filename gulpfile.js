05f33524b92a4c51283dc2e46f8628556b816966
Camille Wintz <camille.wintz@web-education.net>
2015-12-04 10:04:22 +0100

    added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+﻿var gulp = require('gulp');
+var less = require('gulp-less');
+
+gulp.task('skin-leo', function () {
+    gulp.src('./assets/themes/leo/default/*.less')
+      .pipe(less())
+      .pipe(gulp.dest('./assets/themes/leo/default'));
+
+    gulp.src('./assets/themes/leo/dyslexic/*.less')
+      .pipe(less())
+      .pipe(gulp.dest('./assets/themes/leo/dyslexic'));
+});
