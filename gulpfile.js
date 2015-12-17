3a4110397da67756d290f8afc19a6ff2726a07a2
Camille Wintz <camille.wintz@web-education.net>
2015-12-17 17:48:26 +0100

    forgot dyslexic theme

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,10 @@ gulp.task('theme-leo', ['update-libs'], function () {
     gulp.src('./assets/themes/leo/default/*.less')
       .pipe(less())
       .pipe(gulp.dest('./assets/themes/leo/default'));
+
+    gulp.src('./assets/themes/leo/dyslexic/*.less')
+      .pipe(less())
+      .pipe(gulp.dest('./assets/themes/leo/dyslexic'));
 });
 
 //copy themes elements on local to see tests
