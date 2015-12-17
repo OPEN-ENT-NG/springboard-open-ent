37e0deaa85c17a4d65e3ff2b649f20a987bae159
Camille Wintz <camille.wintz@web-education.net>
2015-12-17 17:44:54 +0100

    clean tasks

--- gulpfile.js
+++ gulpfile.js
@@ -11,31 +11,8 @@ gulp.task('theme-leo', ['update-libs'], function () {
     gulp.src('./assets/themes/leo/default/*.less')
       .pipe(less())
       .pipe(gulp.dest('./assets/themes/leo/default'));
-
-    gulp.src('./assets/themes/leo/dyslexic/*.less')
-      .pipe(less())
-      .pipe(gulp.dest('./assets/themes/leo/dyslexic'));
 });
 
-gulp.task('copy-theme-leo', function () {
-    return gulp.src(['../theme-leo/**/*'])
-        .pipe(gulp.dest('./assets/themes/leo'));
-});
-
-gulp.task('copy-css-lib', function () {
-    return gulp.src(['../css-lib/**/*'])
-        .pipe(gulp.dest('./assets/themes/entcore-css-lib'));
-});
-
-gulp.task('theme-leo-local', ['copy-theme-leo', 'copy-css-lib'], function () {
-    gulp.src('./assets/themes/leo/default/*.less')
-      .pipe(less())
-      .pipe(gulp.dest('./assets/themes/leo/default'));
-
-    gulp.src('./assets/themes/leo/dyslexic/*.less')
-      .pipe(less())
-      .pipe(gulp.dest('./assets/themes/leo/dyslexic'));
-});
 //copy themes elements on local to see tests
 gulp.task('copy-theme-leo', function () {
     return gulp.src(['../theme-leo/**/*'])
