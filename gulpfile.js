10f8fa3709361e7e411fdaa803f2bda2ae89b4ca
Camille Wintz <camille.wintz@web-education.net>
2015-12-17 18:13:42 +0100

    copy all themes

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,10 @@ gulp.task('update-libs', function () {
       .pipe(gulp.dest('./assets/themes'));
 });
 
-gulp.task('theme-leo', ['update-libs'], function () {
-    gulp.src('./assets/themes/leo/default/*.less')
+gulp.task('themes', ['update-libs'], function () {
+    return gulp.src('./assets/themes/**/theme.less')
       .pipe(less())
-      .pipe(gulp.dest('./assets/themes/leo/default'));
-
-    gulp.src('./assets/themes/leo/dyslexic/*.less')
-      .pipe(less())
-      .pipe(gulp.dest('./assets/themes/leo/dyslexic'));
+      .pipe(gulp.dest('./assets/themes'));
 });
 
 //copy themes elements on local to see tests
