5aed0a03cea175644dd8fcc5f2b9f27322f46819
Camille Wintz <camille.wintz@web-education.net>
2016-06-01 17:35:22 +0200

    removed widgets from springboard

--- gulpfile.js
+++ gulpfile.js
@@ -1,110 +1,101 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-//var watch = require('gulp-watch');
 var bower = require('gulp-bower');
 var del = require('del');
 var autoprefixer = require('gulp-autoprefixer');
+var merge = require('merge2');
 
+var themeDependencies = ['theme-open-ent', 'entcore-css-lib'];
+var widgets = ['notes', 'calendar-widget', 'record-me'];
 
 var childTheme = 'leo';
-var parentTheme = '/theme-open-ent/**/*';
+var parentTheme = 'theme-open-ent';
 
-var themePath = './assets/themes';
-var localcsslib = '../entcore-css-lib/**/*';
-var localparentTheme = '../theme-open-ent/**/*';
+var localPaths = {
+    themes: ['../entcore-css-lib/**/*', '../theme-open-ent/**/*'],
+    widgets: ['../notes/**/*', '../calendar-widget/**/*', '../record-me/**/*']
+};
 
 var sourceDependency = [];
 
+function widgetsSources() {
+    var sources = [];
+    widgets.forEach((dep) => {
+        sources.push('./bower_components/' + dep + '/**/*');
+    });
+    return sources;
+}
+
+function themesSources() {
+    var sources = [];
+    themeDependencies.forEach((dep) => {
+        sources.push('./bower_components/' + dep + '/**/*');
+    });
+    return sources;
+}
+
 gulp.task('clean', function(){
-    return del([themePath + '/*', '!' + themePath + '/' + childTheme, themePath + '/' + childTheme + '/*',  '!' + themePath + '/' + childTheme + '/override*']);
+    return del([
+        './assets/themes/*',
+        '!./assets/themes/' + childTheme,
+        './assets/themes/' + childTheme + '/*',
+        '!./assets/themes/' + childTheme + '/override*'
+    ]);
 });
 
-//recupere theme sur git & copy dans springboard
-gulp.task('update-libs', ['clean'], function () {
-    return bower({ directory: themePath, cwd: '.' });
+gulp.task('bower', ['clean'], () => {
+    return bower({ cwd: '.' });
 });
 
-//copy dans childTheme ce qu'il a recup du theme (git)
+gulp.task('update', ['bower'], function () {
+    var themes = gulp.src(themesSources(), { base: './bower_components' })
+        .pipe(gulp.dest('./assets/themes'));
+    
+    var widgets = gulp.src(widgetsSources(), { base: './bower_components' })
+        .pipe(gulp.dest('./assets/widgets'));
+
+    return merge([themes, widgets]);
+});
+
 gulp.task('fill-theme', sourceDependency, function () {
-    return gulp.src(themePath + parentTheme)
-        .pipe(gulp.dest(themePath + '/' + childTheme));
+    return gulp.src('./assets/themes/' + parentTheme + '/**/*')
+        .pipe(gulp.dest('./assets/themes/' + childTheme));
 });
 
-
-////////////////////////
-
-//copy dans childTheme les dependances locales
 gulp.task('copy-local', function () {
-    //copy css-lib local
-    return gulp.src([localcsslib, localparentTheme], {base: '../'})
-        .pipe(gulp.dest(themePath));
+    var themes = gulp.src(localPaths.themes, {base: '../'})
+        .pipe(gulp.dest('./assets/themes'));
+    var widgets = gulp.src(localPaths.widgets, { base: '../' })
+        .pipe(gulp.dest('./assets/widgets'));
+    return merge([themes, widgets]);
 });
 
-////////////////////////
-
-
-//override dans childTheme les el specifique
 gulp.task('override-theme', ['fill-theme'], function () {
-    gulp.src([themePath + '/' + childTheme+'/override-img/**/*'])
-        .pipe(gulp.dest(themePath + '/' + childTheme + '/img'));
-
-    gulp.src([themePath + '/' + childTheme + '/override-js/**/*'])
-        .pipe(gulp.dest(themePath + '/' + childTheme+'/js'));
-
-    gulp.src([themePath + '/' + childTheme + '/override-template/**/*'])
-        .pipe(gulp.dest(themePath + '/' + childTheme + '/template'));
+    var overrides = ['img', 'js', 'fonts', 'template', 'default'];
+    overrides.forEach((override) => {
+        gulp.src(['./assets/themes/' + childTheme + '/override-' + override + '/**/*'])
+            .pipe(gulp.dest('./assets/themes/' + childTheme + '/' + override));
+    });
 });
 
-
-//compile sass
 gulp.task('compile-sass', ['override-theme'], function () {
-    //compile le css specifique dans le fichier default et ecrase le theme.css
-    gulp.src(themePath + '/' + childTheme + '/override-css/default/theme.scss')
+    gulp.src('./assets/themes/' + childTheme + '/default/theme.scss')
       .pipe(sass({ outputStyle: 'compressed' }))
       .pipe(autoprefixer())
-      .pipe(gulp.dest(themePath + '/' + childTheme + '/default'));
+      .pipe(gulp.dest('./assets/themes/' + childTheme + '/default'));
 
-    gulp.src(themePath + '/' + childTheme + '/dyslexic/*.scss')
+    gulp.src('./assets/themes/' + childTheme + '/dyslexic/*.scss')
       .pipe(sass({ outputStyle: 'compressed' }))
       .pipe(autoprefixer())
-      .pipe(gulp.dest(themePath + '/' + childTheme + '/dyslexic'));
+      .pipe(gulp.dest('./assets/themes/' + childTheme + '/dyslexic'));
 });
 
 
-gulp.task('themes-local', function(){
+gulp.task('build-local', function(){
     sourceDependency.push('copy-local')
     return gulp.start('compile-sass')
 });
-gulp.task('themes', function(){
-    sourceDependency.push('update-libs')
-    return gulp.start('compile-sass')
+gulp.task('build', function(){
+    sourceDependency.push('update');
+    return gulp.start('compile-sass');
 });
-
-//attention supprimer le theme open-ent si on switch entre les 2 taches
-
-
-
-
-// //watcher
-// gulp.task('watch-css', function() {
-//
-//     function compileTheme() {
-//         // //compile css-lib local
-//         // gulp.src(['../css-lib/**/*'])
-//         //     .pipe(gulp.dest('./assets/themes/entcore-css-lib'));
-//         //
-//         // //compile theme-open-ent local
-//         // gulp.src(['../theme-open-ent/**/*'])
-//         //     .pipe(gulp.dest('./assets/themes/theme-open-ent'));
-//
-//         //compile childTheme
-//         gulp.src('./assets/themes/'+childTheme+'/default/theme.scss')
-//             .pipe(sass())
-//             .pipe(gulp.dest('./assets/themes/'+childTheme+'/default'));
-//     }
-//     watch('../css-lib/**/*.scss', compileTheme);
-//     //watch theme-open-ent local
-//     watch('../theme-open-ent/**/*.scss', compileTheme);
-//     watch('./assets/themes/'+childTheme+'/**/*.scss', compileTheme);
-//
-// });
